5113d49d0c5d954cfbed952894977febe747e18c
TomasKvaraciejus <tom.kvaraciejus@gmail.com>
2024-05-13 13:02:57 +0300

    Added async communication

---
+++ src/main/java/usecases/GenreSongLoader.java
@@ -0,0 +1,33 @@
+package usecases;
+
+import entities.Genre;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.inject.Model;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+@Model
+@SessionScoped
+public class GenreSongLoader implements Serializable
+{
+    @Getter
+    @Setter
+    private Map<Long, CompletableFuture> genresWithLoadedSongs = new HashMap<>();
+
+    public void loadGenreSongs(Long genreId)
+    {
+        genresWithLoadedSongs.put(genreId, CompletableFuture.runAsync(() -> loadSongsAsync()));
+    }
+
+    private void loadSongsAsync(){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+        }
+    }
+}
--- src/main/java/usecases/Genres.java
+++ src/main/java/usecases/Genres.java
@@ -6,11 +6,18 @@ import lombok.Setter;
 import persistence.GenreDAO;
 
 import javax.annotation.PostConstruct;
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.Model;
 import javax.inject.Inject;
+import javax.persistence.PostLoad;
 import javax.transaction.Transactional;
 import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
 
 @Model
 public class Genres implements Serializable {
